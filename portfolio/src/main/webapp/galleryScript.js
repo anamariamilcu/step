d98978d9410a96a07454a3ff8f9df561f6c1aad0
Ana-Maria Milcu <anamariamilcu@gmail.com>
2020-07-17 09:50:23 +0000

    added descriptions and numbering for images

--- portfolio/src/main/webapp/galleryScript.js
+++ portfolio/src/main/webapp/galleryScript.js
@@ -1,35 +1,38 @@
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openGallery() {
+	document.getElementById('myGallery').style.display = 'block';
 }
 
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeGallery() {
+	document.getElementById('myGallery').style.display = 'none';
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function moveNSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 function showCurrentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var descriptionText = document.getElementById("description");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  descriptionText.innerHTML = slides[slideIndex-1].alt;
+	var i;
+	var slides = document.getElementsByClassName('slide');
+	var descriptionText = document.getElementById('description');
+    var slideNumber = document.getElementById('slide-number');
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	slides[slideIndex - 1].style.display = 'block';
+    descriptionText.style.fontSize = 'x-large';
+	descriptionText.innerHTML = slides[slideIndex - 1].getElementsByTagName('img')[0].alt;
+    slideNumber.innerHTML = slideIndex + '/' + slides.length;
 }
